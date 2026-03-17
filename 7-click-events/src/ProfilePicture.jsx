53ff22cbe9a27a645576843c84ec67fe93c9cb47
Ryle-Tan <francesryletan0623@gmail.com>
2026-03-18 02:39:47 +0800

    Finished lesson 7 on click events

---
+++ 7-click-events/src/ProfilePicture.jsx
@@ -0,0 +1,14 @@
+
+
+function ProfilePicture() {
+
+    const imageUrl = './src/assets/profilepic.jpg'
+
+    const handleClick = (e) => e.target.style.display = "none";
+
+    return(
+        <img src={imageUrl} onClick={(e) => handleClick(e)}></img>
+    )
+}
+
+export default ProfilePicture
